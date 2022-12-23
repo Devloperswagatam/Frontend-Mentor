2b0e75ccfe5093f66afa7f3a0d4496088c38a66c
Devloperswagatam <118425263+Devloperswagatam@users.noreply.github.com>
2022-12-24 03:03:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
 const btn4 = document.getElementById('btn4');
 
+const shadowbox = document.querySelector('.shadow');
+const closebtn = document.querySelector('.icon');
+const body = document.querySelector('body');
+
+const pic1 = document.querySelector('.pic1');
+const pic2 = document.querySelector('.pic2');
+const pic3 = document.querySelector('.pic3');
+const pic4 = document.querySelector('.pic4');
+
 
 btn1.addEventListener('click', ()=>{
     img1.style.display = 'block';
@@ -24,6 +33,7 @@ btn1.addEventListener('click', ()=>{
     btn2.classList.remove('active');
     btn3.classList.remove('active');
     btn4.classList.remove('active');
+
 
 })
 
@@ -62,6 +72,8 @@ btn3.addEventListener('click', ()=>{
     btn3.classList.add('active');
     btn4.classList.remove('active');
 
+   
+
 
 })
 
@@ -80,5 +92,77 @@ btn4.addEventListener('click', ()=>{
     btn2.classList.remove('active');
     btn3.classList.remove('active');
     btn4.classList.add('active');
+
+    
 })
 // END OF BUTTON DESIGN
+
+
+img1.addEventListener('click', ()=>{
+    shadowbox.style.display = 'block';
+    body.style.overflow = 'hidden';
+    pic1.style.display = 'block';
+})
+
+img2.addEventListener('click', ()=>{
+    shadowbox.style.display = 'block';
+    body.style.overflow = 'hidden';
+    pic2.style.display = 'block';
+})
+
+img3.addEventListener('click', ()=>{
+    shadowbox.style.display = 'block';
+    body.style.overflow = 'hidden';
+    pic3.style.display = 'block';
+})
+
+img4.addEventListener('click', ()=>{
+    shadowbox.style.display = 'block';
+    body.style.overflow = 'hidden';
+    pic4.style.display = 'block';
+})
+
+closebtn.addEventListener('click', ()=>{
+    shadowbox.style.display = 'none';
+    body.style.overflow = 'scroll';
+})
+
+//END OF OPEN CLOSE BTN
+
+const img = document.querySelectorAll('.img');
+const right = document.getElementById('right');
+const left = document.getElementById('left');
+let count = 0;
+
+
+right.addEventListener('click',(e)=>{
+    img[count].style.display = 'none';
+    count++;
+    img[count].style.display = 'block';
+    if(count===3){
+        right.disabled = true;
+        right.style.cursor = 'not-allowed';
+        right.style.backgroundColor = 'transparent';
+    }
+    left.disabled = false;
+    left.style.cursor = 'pointer';
+    left.style.backgroundColor = '#dfe6e9';    
+});
+
+left.addEventListener('click',(e)=>{
+    img[count].style.display = 'none';
+    count--;
+    img[count].style.display = 'block';
+    if(count===0){
+        left.disabled = true;
+        left.style.cursor = 'not-allowed';
+        left.style.backgroundColor = 'transparent';
+    }
+    right.disabled = false;
+    right.style.cursor = 'pointer';
+    right.style.backgroundColor = '#dfe6e9';
+});
+
+    
+
+
